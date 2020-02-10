290cd52866d7d27955812d94d5f5d80b0c728a18
Alan Lee <alan@coil.com>
2020-02-10 09:59:08 -0800

    init: nodejs project

---
+++ services/hello-world-nodejs/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+app.get('/', (req:any, res:any) => {
+  res.send(`
+  <html>
+    <h1>Hello World</h1>
+  </html>`)
+})
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
